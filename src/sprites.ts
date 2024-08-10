92d81b67a0d41d0080a18f5c87dee94675f45f66
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-10 14:10:52 +0100

    Add type-safe sprites

--- src/sprites.ts
+++ src/sprites.ts
@@ -1,11 +1,17 @@
 import { KAPLAYCtx } from "kaplay";
 
-export enum Sprite {
-  Bean = "sprites/bean.png",
-}
+export const Sprite = {
+  Bean: "Bean",
+} as const;
+
+type SpriteType = (typeof Sprite)[keyof typeof Sprite];
+
+const SpritePaths: Record<SpriteType, string> = {
+  Bean: "sprites/bean.png",
+};
 
 export function loadSprites(k: KAPLAYCtx) {
-  Object.entries(Sprite).forEach(([key, value]) => {
+  Object.entries(SpritePaths).forEach(([key, value]) => {
     k.loadSprite(key, value);
   });
 }
